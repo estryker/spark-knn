ec5ae1578c5c13c3c54974439a761f1885ae5ed3
estryker <e.stryker@gmail.com>
2020-05-07 20:24:32 -0400

    reverted to using scala 2.11 to work properly with my version of spark

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,8 @@ import Keys._
 
 object Dependencies {
   val Versions = Seq(
-    crossScalaVersions := Seq("2.12.8", "2.11.12"),
+    // estryker, test reverting to scala 2.11.12 crossScalaVersions := Seq("2.12.8", "2.11.12"),
+    crossScalaVersions := Seq("2.11.12"),
     scalaVersion := crossScalaVersions.value.head
   )
 
